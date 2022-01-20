b8a827c2c37167773df5e8123ea583cc11ad3d8d
Jorawar Singh <jorawarsinghnijjar@gmail.com>
2022-01-20 16:17:30 -0500

    refactor setIdToken

--- src/app/service/user/user.service.ts
+++ src/app/service/user/user.service.ts
@@ -3,7 +3,7 @@ import { Observable } from 'rxjs';
 import firebase from 'firebase/compat/app';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { environment } from 'src/environments/environment';
 import { User } from 'src/app/model/User';
 
@@ -14,7 +14,7 @@ import { User } from 'src/app/model/User';
 export class UserService implements CanActivate {
 
   user: Observable<firebase.User | null>;
-  
+
 
 
   constructor(private firebaseAuth: AngularFireAuth, private router: Router, private http: HttpClient) {
@@ -23,7 +23,7 @@ export class UserService implements CanActivate {
     this.user.subscribe(
       userInfo => {
         if (userInfo) {
-          this.setIdToken(userInfo.getIdToken());
+          this.setIdToken(userInfo);
         }
       }
     )
@@ -35,52 +35,59 @@ export class UserService implements CanActivate {
     console.log('AuthWithRedirectGuard.canActivate');
     const authenticated = await this.isAuthenticated();
     console.log('canActivate await value: ' + authenticated);
-    
+
     if (!authenticated) {
       this.router.navigate(['login']);
     }
     return authenticated;
 
   }
-  
-  
-  async isAuthenticated(): Promise<boolean>{
-    if(this.user){
+
+
+  async isAuthenticated(): Promise<boolean> {
+    if (this.user) {
       console.log("isAuthenticated")
       return true;
     }
-    else{
+    else {
       console.log("unAuthorized")
       return false;
     }
   }
 
-  setIdToken(idToken: Promise<string>) {
-    idToken.then(token => {
-      localStorage.setItem('userIdToken', token);
-    })
-      .catch(err => {
-        console.log(err);
+  setIdToken(firebaseUser: firebase.User | null) {
+    if (firebaseUser) {
+      firebaseUser.getIdToken().then(token => {
+        localStorage.setItem('userIdToken', token);
       })
+        .catch(err => {
+          console.log(err);
+        })
+
+    }
+    else {
+      throw new Error("Firebase User is null");
+    }
 
   }
 
-  register(email: string, name: string){
-    const user = new User(email,name);
+  register(email: string, name: string) {
+    const user = new User(email, name);
     user.profilePhotoUrl = "https://cdn-icons-png.flaticon.com/512/147/147140.png";
     this.http.post(`${environment.API_URL}/users`, user)
-    .subscribe(res => {
-      this.router.navigate(['albums/recent']);
-      console.log("Success registration");
-    });
+      .subscribe(res => {
+        this.router.navigate(['albums/recent']);
+        console.log("Success registration");
+      });
   }
 
   signIn(email: string, password: string) {
-     this.firebaseAuth.signInWithEmailAndPassword(email, password).then(userCredential => {
-       if(userCredential !== null){
+    this.firebaseAuth.signInWithEmailAndPassword(email, password).then(userCredential => {
+      if (userCredential !== null) {
         console.log("Success SignIn");
+        this.setIdToken(userCredential.user);
         this.router.navigate(['albums/recent']);
-       }
+      }
     })
       .catch(err => {
         console.log("Something went wrong: ", err.message);
@@ -90,6 +97,7 @@ export class UserService implements CanActivate {
   signUp(email: string, password: string, name: string) {
     this.firebaseAuth.createUserWithEmailAndPassword(email, password).then(userCredential => {
       this.register(email, name);
+      this.setIdToken(userCredential.user);
       console.log("Success Sign Up");
     })
       .catch(err => {
